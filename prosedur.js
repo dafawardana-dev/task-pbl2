912c80b2429f7dbe6291520d26215d344ab9a07a
unknown <mdafawardana124@gmail.com>
2025-09-04 09:00:29 +0700

    add jobsheet function prosedur

---
+++ prosedur.js
@@ -0,0 +1,7 @@
+function createSiswa(nama, umur, kelas) {
+    console.log("Nama :", nama);
+    console.log("Umur :", umur);
+    console.log("Kelas :", kelas);
+}
+
+createSiswa("Dafa", 23, "Web");
